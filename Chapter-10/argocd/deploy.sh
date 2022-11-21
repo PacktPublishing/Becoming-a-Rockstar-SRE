bbe23555431590a7c90e397aaec4d06a547ebf16
Rod Anami <rod.anami@br.ibm.com>
2022-11-21 10:58:18 -0300

    Added comments, anonymize commands

--- Chapter-10/argocd/deploy.sh
+++ Chapter-10/argocd/deploy.sh
@@ -1,13 +1,14 @@
-#!/bin/bashß
+#!/bin/bash
 
+# Read the defined environmental variables
 source process.env
-
+# Get the initial admin password, you should change that
 ARGOCD_ADMIN_PASS=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
-
+# Get the IP address for the LoadBalancer service, it assumes you are using a K8s cluster using a cloud platform
 ARGOCD_SERVER=`kubectl -n argocd get svc argocd-server -o jsonpath="{.status.loadBalancer.ingress[].ip}"`
-
+# Logins into Argo CD server
 argocd login $ARGOCD_SERVER --username $ARGOCD_ADMIN_NAME --password $ARGOCD_ADMIN_PASS --insecure
-
+# Creates an Argo CD app based on the variables from process.env
 argocd app create $APP_NAME --repo $APP_REPO_URL \
  --path $APP_REPO_PATH --dest-server https://kubernetes.default.svc \
  --dest-namespace default
--- Chapter-10/argocd/install.sh
+++ Chapter-10/argocd/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-gcloud container clusters get-credentials cluster-1 --zone southamerica-west1-a --project observability-simulation-lab
-
+# Configure kubectl to connect to a GKE cluster
+gcloud container clusters get-credentials cluster-1 --zone <your_closest_region> --project <your_project_id>
+# Create a namespace for Argo CD
 kubectl create namespace argocd
+# Deploy Argo CD server
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# Change Argo CD service to LoadBalancer type
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
